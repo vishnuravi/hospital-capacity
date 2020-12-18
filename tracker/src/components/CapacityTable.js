3826be687992ecf8a4d4d5f496bd2bcfc764b542
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-12-18 09:46:46 -0500

    Update metadata

--- tracker/src/components/CapacityTable.js
+++ tracker/src/components/CapacityTable.js
@@ -27,8 +27,6 @@ export default function CapacityTable() {
             const results = await axios.get(`${process.env.REACT_APP_API_URL}/hospitals/state/${state.value}`);
             const data = results.data;
 
-            console.log(data);
-
             // extract only acute care facilities
             const acuteCareData = data.filter((row) => row.hospital_subtype.toLowerCase() === "short term" || row.hospital_subtype.toLowerCase() === "critical access hospitals");
 
