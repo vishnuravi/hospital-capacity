446ef5cdd3fb21118811b7a6ecfbddba25ad6a42
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-12-30 01:28:57 -0500

    Add data update frequency

--- tracker/src/components/Header.js
+++ tracker/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <h1 className="text-center my-4">Hospital Capacity Tracker</h1>
             <p className="text-center">An <a href="https://github.com/vishnuravi/hospital-capacity" target="_blank" rel="noreferrer">open source project</a> by <a href="https://vishnu.io" target="_blank" rel="noreferrer">Vishnu Ravi</a> with data from <a href="https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-facility" target="_blank">HealthData.gov</a>.</p>
-
+            <p className="text-center">Data is averaged weekly and new data is published by HHS every Monday.</p>
         </>
     )
 }
