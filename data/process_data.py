02e0859d962ce3a4683bc2c6d00d5d9281285753
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-01-02 16:17:20 -0500

    Updated data processing scripts
    Data processing scripts now grab URL to latest CSV file from healthdata.gov dataset metadata.

--- data/process_data.py
+++ data/process_data.py
@@ -3,6 +3,7 @@ import urllib.request
 import mysql.connector
 import yaml
 import os
+import json
 
 with open("config.yml", "r") as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -10,23 +11,24 @@ with open("config.yml", "r") as ymlfile:
 
 # Download dataset from healthcare.gov
 print('Starting csv file download...')
-
-url_to_csv = config["dataset"]["url"]
+url = config["dataset"]["url"]
+dataset_metadata = urllib.request.urlopen(url).read().decode()
+json = json.loads(dataset_metadata)
+url_to_csv = json["result"][0]["resources"][0]["url"]
 urllib.request.urlretrieve(url_to_csv, 'latest-data.csv')
 
 # Extract columns we need
 print('Extracting columns from csv...')
-
 columns_to_keep = config["dataset"]["columns"]
-
 df = pd.read_csv('latest-data.csv', dtype=str)
 df = df[columns_to_keep]
 df.to_csv("processed-data.csv", index=False, index_label=False)
 os.remove('latest-data.csv')
 
-print('Inserting new data into database...')
-
-cnx = mysql.connector.connect(user=config["mysql"]["user"], passwd=config["mysql"]["passwd"], host=config["mysql"]["host"], database=config["mysql"]["db"], allow_local_infile=True)
+# Update database
+print('Updating data in database...')
+cnx = mysql.connector.connect(user=config["mysql"]["user"], passwd=config["mysql"]["passwd"],
+                              host=config["mysql"]["host"], database=config["mysql"]["db"], allow_local_infile=True)
 cursor = cnx.cursor()
 load_data_query = "LOAD DATA LOCAL INFILE 'processed-data.csv' INTO TABLE CapacityData FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
 cursor.execute("DELETE FROM CapacityData;")
