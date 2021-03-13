39c099a080259ec799b2689a44f7569ab1710f6c
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-03-12 20:44:47 -0500

    Update dataset URI

--- tracker/src/components/Footer.js
+++ tracker/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="mt-4">
             <hr />
-            <p className="text-center footer-text">Metrics shown here are based on data aggregated and published by the U.S. Department of Health &amp; Human Services weekly <a href="https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-facility" target="_blank">(link)</a>. <br /> This website is not affiliated with the U.S. government or any other entity, and should not be used to guide decisions about seeking care.</p>
+            <p className="text-center footer-text">Metrics shown here are based on data aggregated and published by the U.S. Department of Health &amp; Human Services weekly <a href="https://healthdata.gov/Hospital/COVID-19-Reported-Patient-Impact-and-Hospital-Capa/anag-cw7u" target="_blank">(link)</a>. This website is not affiliated with the U.S. government or any other entity, and should not be used to guide decisions about seeking care.</p>
         </div>
     )
 }
--- tracker/src/components/Header.js
+++ tracker/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <>
             <h1 className="text-center my-4">Hospital Capacity Tracker</h1>
-            <p className="text-center">An <a href="https://github.com/vishnuravi/hospital-capacity" target="_blank" rel="noreferrer">open source project</a> by <a href="https://vishnu.io" target="_blank" rel="noreferrer">Vishnu Ravi</a> using data from <a href="https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-facility" target="_blank">HealthData.gov</a>.</p>
+            <p className="text-center">An <a href="https://github.com/vishnuravi/hospital-capacity" target="_blank" rel="noreferrer">open source project</a> by <a href="https://vishnu.io" target="_blank" rel="noreferrer">Vishnu Ravi</a> using data from <a href="https://healthdata.gov/Hospital/COVID-19-Reported-Patient-Impact-and-Hospital-Capa/anag-cw7u" target="_blank">HealthData.gov</a>.</p>
         </>
     )
 }
