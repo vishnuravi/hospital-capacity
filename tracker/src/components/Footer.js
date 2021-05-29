3d818cdacc72df29a8d03e775f458d4aa95821c6
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-05-29 16:48:06 +0000

    Minor design updates

--- tracker/src/components/Footer.js
+++ tracker/src/components/Footer.js
@@ -1,10 +1,9 @@
-import React from 'react'
-
 export default function Footer() {
     return (
-        <div className="mt-4">
-            <hr />
-            <p className="text-center footer-text">Note: This website is not affiliated with the U.S. government or any other entity, and should not be used to guide decisions about seeking care.</p>
-        </div>
+        <footer className="mt-4 rounded">
+            <div className="text-center text-muted p-3 footer-text">
+                This website is not affiliated with the U.S. government and should not be used to guide decisions about seeking care.
+            </div>
+        </footer>
     )
 }
